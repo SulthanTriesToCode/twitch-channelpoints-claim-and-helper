1eb8387b08501c3daa6b5faee081fe50f1f9865f
Sulthan Syuza Prabowo <86139250+SulthanTriesToCode@users.noreply.github.com>
2025-08-27 16:20:08 +0700

    Add Twitch Auto Claim & Stream Bonus Helper script

---
+++ twitch-channelpoints-claim-and-helper.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Twitch Auto Claim & Stream Bonus Helper
+// @namespace    TwitchScripts
+// @version      2.7
+// @description  Auto-claims channel points, set the quality to low, mutes the stream, and reloads the stream if an error occurs.
+// @author       Domopremo (Original) SulthanTriesToCode (Fork)
+// @match        https://www.twitch.tv/*
+// @icon         https://www.twitch.tv/favicon.ico
+// @grant        none
+// @license      MIT
+// @run-at       document-idle
+// ==/UserScript==
